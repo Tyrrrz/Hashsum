9cea2400d0facf2ca7612a763df104064fa978f3
Alexey Golub <Tyrrrrrr@gmail.com>
2018-01-20 19:08:08 +0200

    Add documentation

--- Hashsum/ChecksumBuilder.cs
+++ Hashsum/ChecksumBuilder.cs
@@ -6,6 +6,9 @@ using Hashsum.Internal;
 
 namespace Hashsum
 {
+    /// <summary>
+    /// Fluent interface for mutating and calculating checksums.
+    /// </summary>
     public class ChecksumBuilder : IDisposable
     {
         private readonly HashAlgorithm _algorithm;
@@ -14,6 +17,9 @@ namespace Hashsum
         private readonly string _separator;
         private readonly IFormatProvider _formatProvider;
 
+        /// <summary>
+        /// Initializes <see cref="ChecksumBuilder" /> with given hashing algorithm.
+        /// </summary>
         public ChecksumBuilder(HashAlgorithm algorithm, bool disposeAlgorithm = true)
         {
             algorithm.GuardNotNull(nameof(algorithm));
@@ -25,6 +31,9 @@ namespace Hashsum
             _formatProvider = CultureInfo.InvariantCulture;
         }
 
+        /// <summary>
+        /// Initializes <see cref="ChecksumBuilder" /> with <see cref="SHA256" /> hashing algorithm.
+        /// </summary>
         public ChecksumBuilder()
             : this(SHA256.Create())
         {
@@ -43,36 +52,81 @@ namespace Hashsum
         private ChecksumBuilder AppendToBuffer(IFormattable value, string format) =>
             AppendToBuffer(value?.ToString(format, _formatProvider));
 
+        /// <summary>
+        /// Mutates checksum by given value.
+        /// </summary>
         public ChecksumBuilder Mutate(string value) => AppendToBuffer(value);
 
+        /// <summary>
+        /// Mutates checksum by given value.
+        /// </summary>
         public ChecksumBuilder Mutate(short value) => AppendToBuffer(value, "D");
 
+        /// <summary>
+        /// Mutates checksum by given value.
+        /// </summary>
         public ChecksumBuilder Mutate(int value) => AppendToBuffer(value, "D");
 
+        /// <summary>
+        /// Mutates checksum by given value.
+        /// </summary>
         public ChecksumBuilder Mutate(long value) => AppendToBuffer(value, "D");
 
+        /// <summary>
+        /// Mutates checksum by given value.
+        /// </summary>
         public ChecksumBuilder Mutate(ushort value) => AppendToBuffer(value, "D");
 
+        /// <summary>
+        /// Mutates checksum by given value.
+        /// </summary>
         public ChecksumBuilder Mutate(uint value) => AppendToBuffer(value, "D");
 
+        /// <summary>
+        /// Mutates checksum by given value.
+        /// </summary>
         public ChecksumBuilder Mutate(ulong value) => AppendToBuffer(value, "D");
 
+        /// <summary>
+        /// Mutates checksum by given value.
+        /// </summary>
         public ChecksumBuilder Mutate(Guid value) => AppendToBuffer(value, "D");
 
+        /// <summary>
+        /// Mutates checksum by given value.
+        /// </summary>
         public ChecksumBuilder Mutate(float value) => AppendToBuffer(value, "F");
 
+        /// <summary>
+        /// Mutates checksum by given value.
+        /// </summary>
         public ChecksumBuilder Mutate(double value) => AppendToBuffer(value, "F");
 
+        /// <summary>
+        /// Mutates checksum by given value.
+        /// </summary>
         public ChecksumBuilder Mutate(decimal value) => AppendToBuffer(value, "F");
 
+        /// <summary>
+        /// Mutates checksum by given value.
+        /// </summary>
         public ChecksumBuilder Mutate(DateTime value) => AppendToBuffer(value, "u");
 
+        /// <summary>
+        /// Mutates checksum by given value.
+        /// </summary>
         public ChecksumBuilder Mutate(DateTimeOffset value) => AppendToBuffer(value, "u");
 
+        /// <summary>
+        /// Mutates checksum by given value.
+        /// </summary>
         public ChecksumBuilder Mutate(byte[] value) => AppendToBuffer(Convert.ToBase64String(value));
 
         #endregion
 
+        /// <summary>
+        /// Calculates the checksum and clears buffer.
+        /// </summary>
         public byte[] Calculate()
         {
             var data = Encoding.Unicode.GetBytes(_buffer.ToString());
@@ -80,8 +134,12 @@ namespace Hashsum
             return _algorithm.ComputeHash(data);
         }
 
+        /// <inheritdoc />
         public override string ToString() => _buffer.ToString();
 
+        /// <summary>
+        /// Disposes resources.
+        /// </summary>
         protected virtual void Dispose(bool disposing)
         {
             if (disposing)
@@ -92,6 +150,7 @@ namespace Hashsum
             }
         }
 
+        /// <inheritdoc />
         public void Dispose()
         {
             Dispose(true);
--- Hashsum/Extensions.cs
+++ Hashsum/Extensions.cs
@@ -3,8 +3,14 @@ using Hashsum.Internal;
 
 namespace Hashsum
 {
+    /// <summary>
+    /// Extensions for <see cref="Hashsum"/>.
+    /// </summary>
     public static class Extensions
     {
+        /// <summary>
+        /// Calculates checksum and converts it to base64 string.
+        /// </summary>
         public static string CalculateToString(this ChecksumBuilder builder)
         {
             builder.GuardNotNull(nameof(builder));
